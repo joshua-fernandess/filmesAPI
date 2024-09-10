fa6c6d9693224bca2ad8573fea141e1d9492c096
adrianelouise <fernandessjosh@gmail.com>
2024-09-09 21:44:39 -0300

    .

--- filmes/buscarFilmes.js
+++ filmes/buscarFilmes.js
@@ -2,13 +2,13 @@ const { filmes } = require('../data');
 
 const buscarFilmes = (req, res) => {
     const { ano } = req.query;
-    const resultados = filmes.filter(filme => filme.ano.includes(ano));
+    const resultados = filmes.filter(filme => filme.ano == ano);
 
     if (resultados.length === 0) {
-        return res.status(404).send({ message: 'Nenhum filme encontrado com esse ano.'}) ;
-        }
-        
-        res.status(200).send(resultados);
+        return res.status(404).send({ message: 'Nenhum filme encontrado com esse ano.' });
+    }
+    
+    res.status(200).send(resultados);
 }
 
 module.exports = buscarFilmes;
